41403c0f07517f84b1ae65b9b50fd338092f35ff
Akibur Rahman <me.akiburrahman@gmail.com>
2023-02-09 17:01:55 +0600

    completed BMI app
    added background color change depending on BMI value

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -35,103 +35,117 @@ class _MyHomePageState extends State<MyHomePage> {
   var weightContorller = TextEditingController(); //takes weight
   dynamic result = '';
 
+  var bgColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Center(child: const Text('BMI Calculator')),
         ),
-        body: Center(
-          child: Container(
-            width: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'BMI',
-                  style: TextStyle(
-                    fontSize: 34,
-                    fontWeight: FontWeight.w600,
+        body: Container(
+          color: bgColor,
+          child: Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'BMI',
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                TextField(
-                  controller: weightContorller,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    label: Text("Enter Your weight in kg"),
-                    prefixIcon: Icon(Icons.line_weight),
+                  SizedBox(
+                    height: 11,
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                TextField(
-                  controller: feetHeightController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    label: Text("Enter Your height in feet"),
-                    prefixIcon: Icon(Icons.man),
+                  TextField(
+                    controller: weightContorller,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      label: Text("Enter Your weight in kg"),
+                      prefixIcon: Icon(Icons.line_weight),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                TextField(
-                  controller: inchHeightController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    label: Text("Enter Your height in inch"),
-                    prefixIcon: Icon(Icons.man_2_sharp),
+                  SizedBox(
+                    height: 11,
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                SizedBox(
-                  height: 41,
-                  width: 150,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      var weight = weightContorller.text.toString();
-                      var feet = feetHeightController.text.toString();
-                      var inch = inchHeightController.text.toString();
-                      // ignore: unnecessary_null_comparison
-                      if (weight != '' && feet != '' && inch != '') {
-                        //calculate BMI
-                        var iwt = int.parse(weight);
-                        var ift = int.parse(feet);
-                        var iInch = int.parse(inch);
+                  TextField(
+                    controller: feetHeightController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      label: Text("Enter Your height in feet"),
+                      prefixIcon: Icon(Icons.man),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  TextField(
+                    controller: inchHeightController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      label: Text("Enter Your height in inch"),
+                      prefixIcon: Icon(Icons.man_2_sharp),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  SizedBox(
+                    height: 41,
+                    width: 150,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        var weight = weightContorller.text.toString();
+                        var feet = feetHeightController.text.toString();
+                        var inch = inchHeightController.text.toString();
+                        // ignore: unnecessary_null_comparison
+                        if (weight != '' && feet != '' && inch != '') {
+                          //calculate BMI
+                          var iwt = int.parse(weight);
+                          var ift = int.parse(feet);
+                          var iInch = int.parse(inch);
 
-                        var tInch = (ift * 12) + iInch;
-                        var tCM = tInch * 2.54;
-                        var tM = tCM / 100;
+                          var tInch = (ift * 12) + iInch;
+                          var tCM = tInch * 2.54;
+                          var tM = tCM / 100;
 
-                        var BMI = iwt / (tM * tM);
+                          var BMI = iwt / (tM * tM);
 
-                        result = 'Your BMI is: ${BMI.toStringAsFixed(2)}';
-                        setState(() {});
-                      } else {
-                        result = 'Please Fillup all the fields';
-                        setState(() {});
-                      }
-                    },
-                    child: Text("Calculate"),
+                          if (BMI > 25) {
+                            bgColor = Colors.red.shade200;
+                          } else if (BMI < 18) {
+                            bgColor = Colors.yellow.shade100;
+                          } else {
+                            bgColor = Colors.green.shade300;
+                          }
+
+                          result = 'Your BMI is: ${BMI.toStringAsFixed(2)}';
+                          setState(() {});
+                        } else {
+                          result = 'Please Fillup all the fields';
+                          bgColor = Colors.red.shade100;
+                          setState(() {});
+                        }
+                      },
+                      child: Text("Calculate"),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                Text(
-                  result,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
+                  SizedBox(
+                    height: 11,
                   ),
-                )
-              ],
+                  Text(
+                    result,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
